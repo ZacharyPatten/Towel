817a4e81aa091f69d81981fc7fb8d32e142ce8df
Zachary Patten <sevenix.zp@gmail.com>
2019-05-17 23:13:09 -0400

    Prepping Measurement Types For Generation

--- Sources/Towel/Measurements/Acceleration.cs
+++ Sources/Towel/Measurements/Acceleration.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace Towel.Measurements
 {
-    class Acceleration
+    /// <summary>Contains unit types and conversion factors for the generic Acceleration struct.</summary>
+    public static class Acceleration
     {
+        /// <summary>Units for acceleration measurements.</summary>
+        [Serializable]
+        public enum Units
+        {
+            // Note: These enum values are critical. They are used to determine
+            // unit priorities and storage of location conversion factors. They 
+            // need to be small and in non-increasing order of unit size.
+
+            //[ConversionFactor(XXXXX, XXXXX, "XXX")]
+            /// <summary>Units of an Acceleration measurement.</summary>
+            //UNITS = X,
+        }
     }
 }
--- Sources/Towel/Measurements/Time.cs
+++ Sources/Towel/Measurements/Time.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace Towel.Measurements
 {
-    class Time
+    /// <summary>Contains unit types and conversion factors for the generic Time struct.</summary>
+    public static class Time
     {
+        /// <summary>Units for time measurements.</summary>
+        [Serializable]
+        public enum Units
+        {
+            // Note: These enum values are critical. They are used to determine
+            // unit priorities and storage of location conversion factors. They 
+            // need to be small and in non-increasing order of unit size.
+
+            //[ConversionFactor(XXXXX, XXXXX, "XXX")]
+            /// <summary>Units of an Time measurement.</summary>
+            //UNITS = X,
+        }
     }
 }
