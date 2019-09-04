65f8bdd7768f83c87d903914db9e1f56e1b9c31f
Zachary Patten <sevenix.zp@gmail.com>
2019-09-03 20:59:07 -0400

    Update Assume.cs

--- Sources/Towel/Assume.cs
+++ Sources/Towel/Assume.cs
@@ -10,84 +10,6 @@ namespace Towel
 	/// <summary>Breaks type safeness by making assumptions about types.</summary>
 	public static class Assume
 	{
-		public static T PropertyGet<T>(object @object, Type type, string propertyName, BindingFlags? bindingFlags) where T : Delegate
-		{
-			PropertyInfo[] propertyInfos =
-				!bindingFlags.HasValue ?
-				type.GetProperties() :
-				type.GetProperties(bindingFlags.Value);
-			foreach (PropertyInfo propertyInfo in propertyInfos)
-			{
-				if (propertyInfo.Name.Equals(propertyName))
-				{
-					try
-					{
-						return
-							@object is null ?
-							(T)propertyInfo.GetGetMethod().CreateDelegate(typeof(T)) :
-							(T)propertyInfo.GetGetMethod().CreateDelegate(typeof(T), @object);
-					}
-					catch
-					{
-						continue;
-					}
-				}
-			}
-			return null;
-		}
-
-		public static T PropertySet<T>(object @object, Type type, string propertyName, BindingFlags? bindingFlags) where T : Delegate
-		{
-			PropertyInfo[] propertyInfos =
-				!bindingFlags.HasValue ?
-				type.GetProperties() :
-				type.GetProperties(bindingFlags.Value);
-			foreach (PropertyInfo propertyInfo in propertyInfos)
-			{
-				if (propertyInfo.Name.Equals(propertyName))
-				{
-					try
-					{
-						return
-							@object is null ?
-							(T)propertyInfo.GetSetMethod().CreateDelegate(typeof(T)) :
-							(T)propertyInfo.GetSetMethod().CreateDelegate(typeof(T), @object);
-					}
-					catch
-					{
-						continue;
-					}
-				}
-			}
-			return null;
-		}
-
-		public static T Method<T>(object @object, Type type, string methodName, BindingFlags? bindingFlags) where T : Delegate
-		{
-			MethodInfo[] methodInfos =
-				!bindingFlags.HasValue ?
-				type.GetMethods() :
-				type.GetMethods(bindingFlags.Value);
-			foreach (MethodInfo methodInfo in methodInfos)
-			{
-				if (methodInfo.Name.Equals(methodName))
-				{
-					try
-					{
-						return
-							@object is null ?
-							(T)methodInfo.CreateDelegate(typeof(T)) :
-							(T)methodInfo.CreateDelegate(typeof(T), @object);
-					}
-					catch
-					{
-						continue;
-					}
-				}
-			}
-			return null;
-		}
-
 		public static bool TryParse<T>(string @string, out T value)
 		{
 			return TryParseImplementation<T>.Function(@string, out value);
@@ -99,14 +21,26 @@ namespace Towel
 
 			internal static TryParseDelegate Function = (string @string, out T value) =>
 			{
-				TryParseDelegate function = Method<TryParseDelegate>(null, typeof(T), "TryParse", BindingFlags.Static);
-				Function = !(function is null) ?
-					function :
+				Type type = typeof(T);
+				Type[] parameterTypes = new Type[] { typeof(string), type.MakeByRefType() };
+				MethodInfo methodInfo =
+					type.GetMethod("TryParse",
+						BindingFlags.Static |
+						BindingFlags.Public |
+						BindingFlags.NonPublic,
+						null,
+						parameterTypes,
+						null);
+				Function =
+					methodInfo is null
+					?
 					(string _string, out T _value) =>
 					{
 						_value = default;
 						return false;
-					};
+					}
+				:
+					(TryParseDelegate)methodInfo.CreateDelegate(typeof(TryParseDelegate));
 				return Function(@string, out value);
 			};
 		}
