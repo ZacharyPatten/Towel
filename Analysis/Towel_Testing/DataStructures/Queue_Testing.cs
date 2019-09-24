f5aa4989c9801310df1089cd61dfa20e6ec3a4b8
Zachary Patten <sevenix.zp@gmail.com>
2019-09-23 21:10:23 -0400

    Data Structure Tests

---
+++ Analysis/Towel_Testing/DataStructures/Queue_Testing.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Towel;
+using Towel.DataStructures;
+
+namespace Towel_Testing.DataStructures
+{
+	[TestClass]
+	public class QueueArray_Testing
+	{
+		[TestMethod]
+		public void Enqueue_Dequeue_Testing()
+		{
+			void Test<T>(T[] values)
+			{
+				Towel.Algorithms.Sort.Shuffle(values);
+				IQueue<T> stack = new QueueArray<T>();
+				values.Stepper(x => stack.Enqueue(x));
+				values.Stepper(x => x.Equals(stack.Dequeue()));
+			}
+
+			{ // int
+				const int count = 100000;
+				int[] values = new int[count];
+				Stepper.Iterate(count, i => values[i] = i);
+				Test(values);
+			}
+			{ // string
+				const int count = 100000;
+				string[] values = new string[count];
+				Stepper.Iterate(count, i => values[i] = i.ToString());
+				Test(values);
+			}
+		}
+	}
+
+	[TestClass]
+	public class QueueLinked_Testing
+	{
+		[TestMethod]
+		public void Enqueue_Dequeue_Testing()
+		{
+			void Test<T>(T[] values)
+			{
+				Towel.Algorithms.Sort.Shuffle(values);
+				IQueue<T> stack = new QueueLinked<T>();
+				values.Stepper(x => stack.Enqueue(x));
+				values.Stepper(x => x.Equals(stack.Dequeue()));
+			}
+
+			{ // int
+				const int count = 100000;
+				int[] values = new int[count];
+				Stepper.Iterate(count, i => values[i] = i);
+				Test(values);
+			}
+			{ // string
+				const int count = 100000;
+				string[] values = new string[count];
+				Stepper.Iterate(count, i => values[i] = i.ToString());
+				Test(values);
+			}
+		}
+	}
+}
---
+++ Analysis/Towel_Testing/DataStructures/Stack_Testing.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Towel;
+using Towel.DataStructures;
+
+namespace Towel_Testing.DataStructures
+{
+	[TestClass]
+	public class StackArray_Testing
+	{
+		[TestMethod]
+		public void Push_Pop_Testing()
+		{
+			void Test<T>(T[] values)
+			{
+				Towel.Algorithms.Sort.Shuffle(values);
+				IStack<T> stack = new StackArray<T>();
+				values.Stepper(x => stack.Push(x));
+				Array.Reverse(values);
+				values.Stepper(x => x.Equals(stack.Pop()));
+			}
+
+			{ // int
+				const int count = 10000;
+				int[] values = new int[count];
+				Stepper.Iterate(count, i => values[i] = i);
+				Test(values);
+			}
+			{ // string
+				const int count = 10000;
+				string[] values = new string[count];
+				Stepper.Iterate(count, i => values[i] = i.ToString());
+				Test(values);
+			}
+		}
+	}
+
+	[TestClass]
+	public class StackLinked_Testing
+	{
+		[TestMethod]
+		public void Push_Pop_Testing()
+		{
+			void Test<T>(T[] values)
+			{
+				Towel.Algorithms.Sort.Shuffle(values);
+				IStack<T> stack = new StackLinked<T>();
+				values.Stepper(x => stack.Push(x));
+				Array.Reverse(values);
+				values.Stepper(x => x.Equals(stack.Pop()));
+			}
+
+			{ // int
+				const int count = 100000;
+				int[] values = new int[count];
+				Stepper.Iterate(count, i => values[i] = i);
+				Test(values);
+			}
+			{ // string
+				const int count = 100000;
+				string[] values = new string[count];
+				Stepper.Iterate(count, i => values[i] = i.ToString());
+				Test(values);
+			}
+		}
+	}
+}
