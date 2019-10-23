e539fa55f3734d31143adc044adf2b3e8145c88a
Zachary Patten <sevenix.zp@gmail.com>
2019-10-22 23:55:01 -0400

    Added Exceptions To Queue Dequeue Tests

--- Analysis/Towel_Testing/DataStructures/Queue.cs
+++ Analysis/Towel_Testing/DataStructures/Queue.cs
@@ -12,11 +12,10 @@ namespace Towel_Testing.DataStructures
 			void Test<T>(T[] values)
 			{
 				Towel.Algorithms.Sort.Shuffle(values);
-				IQueue<T> stack = new QueueArray<T>();
-				values.Stepper(x => stack.Enqueue(x));
-				values.Stepper(x => x.Equals(stack.Dequeue()));
+				IQueue<T> queue = new QueueArray<T>();
+				values.Stepper(x => queue.Enqueue(x));
+				values.Stepper(x => x.Equals(queue.Dequeue()));
 			}
-
 			{ // int
 				const int count = 100000;
 				int[] values = new int[count];
@@ -28,23 +27,25 @@ namespace Towel_Testing.DataStructures
 				string[] values = new string[count];
 				Stepper.Iterate(count, i => values[i] = i.ToString());
 				Test(values);
+			}
+			{ // exceptions
+				IQueue<int> queue = new QueueArray<int>();
+				Assert.ThrowsException<InvalidOperationException>(() => queue.Dequeue());
 			}
 		}
 	}
 
 	[TestClass] public class QueueLinked_Testing
 	{
-		[TestMethod]
-		public void Enqueue_Dequeue_Testing()
+		[TestMethod] public void Enqueue_Dequeue_Testing()
 		{
 			void Test<T>(T[] values)
 			{
 				Towel.Algorithms.Sort.Shuffle(values);
-				IQueue<T> stack = new QueueLinked<T>();
-				values.Stepper(x => stack.Enqueue(x));
-				values.Stepper(x => x.Equals(stack.Dequeue()));
+				IQueue<T> queue = new QueueLinked<T>();
+				values.Stepper(x => queue.Enqueue(x));
+				values.Stepper(x => x.Equals(queue.Dequeue()));
 			}
-
 			{ // int
 				const int count = 100000;
 				int[] values = new int[count];
@@ -56,6 +57,10 @@ namespace Towel_Testing.DataStructures
 				string[] values = new string[count];
 				Stepper.Iterate(count, i => values[i] = i.ToString());
 				Test(values);
+			}
+			{ // exceptions
+				IQueue<int> queue = new QueueLinked<int>();
+				Assert.ThrowsException<InvalidOperationException>(() => queue.Dequeue());
 			}
 		}
 	}
