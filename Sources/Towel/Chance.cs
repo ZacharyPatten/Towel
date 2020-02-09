30ea44a957b6eaf07c3a97d943b15c4fa40d1118
Zachary Patten <sevenix.zp@gmail.com>
2020-02-08 19:04:25 -0500

    Chance Clean Up

--- Sources/Towel/Chance.cs
+++ Sources/Towel/Chance.cs
@@ -2,15 +2,27 @@
 
 namespace Towel
 {
-    public struct ChanceSyntax
-    {
-        public static Random Algorithm = new Random();
-        //public static ChanceSyntax Chance => default;
-        public static bool operator %(double value, ChanceSyntax chance) =>
-            value < 0 ? throw new ArgumentOutOfRangeException(nameof(chance)) :
-            value > 100 ? throw new ArgumentOutOfRangeException(nameof(chance)) :
-            value is 100 ? true :
-            value is 0 ? false :
-            Algorithm.NextDouble() < value / 100;
-    }
+	/// <summary>Struct that allows percentage syntax that will be evaluated at runtime.</summary>
+	public struct ChanceSyntax
+	{
+		/// <summary>The random algorithm currently being used by chance syntax.</summary>
+		public static Random Algorithm = new Random();
+
+		//public static ChanceSyntax Chance => default;
+
+#pragma warning disable IDE0060 // Remove unused parameter
+
+		/// <summary>Creates a chance from a percentage that will be evaluated at runtime.</summary>
+		/// <param name="percentage">The value of the percentage.</param>
+		/// <param name="chance">The chance syntax struct object.</param>
+		/// <returns></returns>
+		public static bool operator %(double percentage, ChanceSyntax chance) =>
+			percentage < 0 ? throw new ArgumentOutOfRangeException(nameof(chance)) :
+			percentage > 100 ? throw new ArgumentOutOfRangeException(nameof(chance)) :
+			percentage is 100 ? true :
+			percentage is 0 ? false :
+			Algorithm.NextDouble() < percentage / 100;
+
+#pragma warning restore IDE0060 // Remove unused parameter
+	}
 }
