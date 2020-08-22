16e66a6d485ac38537721552b572c16bd33696c8
Zachary Patten <sevenix.zp@gmail.com>
2020-08-22 01:14:02 -0400

    #37 Omnitree Rewrite Start

---
+++ Tools/Towel_Generating/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Towel_Generating
+{
+	internal class Program
+	{
+		internal static void Main()
+		{
+			Console.WriteLine("This is a code generation tool for Towel.");
+			Console.WriteLine();
+			Console.WriteLine("I'm working to replace the T4 templates with with this tool for code generation.");
+			Console.WriteLine();
+			Omnitree.Generate();
+		}
+	}
+}
