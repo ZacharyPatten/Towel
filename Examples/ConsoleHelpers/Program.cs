91068e48e5502be570e0a3aeb92aa1306a4dab97
Zachary Patten <sevenix.zp@gmail.com>
2020-09-10 20:24:56 -0400

    ConsoleHelpers

---
+++ Examples/ConsoleHelpers/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Threading;
+using Towel;
+
+namespace ConsoleHelpers
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("You are runnning the ConsoleHelpers example.");
+			Console.WriteLine("============================================");
+			Console.WriteLine();
+
+			#region Prompt Press To Continue Example
+			{
+				Console.WriteLine("---------------------");
+				Console.WriteLine("PromptPressToContinue is an reasy method");
+				Console.WriteLine("to wait for user input to continue.");
+				ConsoleHelper.PromptPressToContinue();
+				Console.WriteLine();
+				Console.WriteLine();
+			}
+			#endregion
+
+			#region Get Input Example
+			{
+				Console.WriteLine("---------------------");
+				Console.WriteLine("GetInput is an reasy method to loop until the");
+				Console.WriteLine("user provides valid input of a generic type.");
+				double double_intput = ConsoleHelper.GetInput<double>();
+				Console.WriteLine($"Your input: {double_intput}.");
+				int min = 0;
+				int max = 100;
+				int int_input = ConsoleHelper.GetInput<int>(
+					prompt: $"Insert an integer ({min}-{max}): ",
+					validation: value => min <= value && value <= max);
+				Console.WriteLine($"Your input: {int_input}.");
+				Console.WriteLine();
+			}
+			#endregion
+
+			#region Animated Ellipsis Example
+			{
+				Console.WriteLine("---------------------");
+				Console.WriteLine("Animated Ellipsis Example");
+				Console.Write("Press Enter to start");
+				ConsoleHelper.PressToContinue();
+				DoWith(cursorVisible: false, action: () =>
+				{
+					Thread thread = new Thread(() => Thread.Sleep(TimeSpan.FromSeconds(5)));
+					thread.Start();
+					ConsoleHelper.AnimatedElipsis(
+						condition: () => thread.IsAlive,
+						delay: () => Thread.Sleep(TimeSpan.FromMilliseconds(300)));
+				});
+				Console.WriteLine("...");
+				Console.WriteLine();
+			}
+			#endregion
+
+			#region Progress Bar Example
+			{
+				Console.WriteLine("---------------------");
+				Random random = new Random();
+				Console.WriteLine("Progress Bar Example.");
+				ConsoleHelper.PromptPressToContinue();
+				Console.WriteLine();
+				DoWith(cursorVisible: false, action: () =>
+					{
+						ConsoleHelper.ProgressBar(
+							postClear: false,
+							length: 26,
+							action: action =>
+							{
+								int iterations = random.Next(5, 20);
+								for (int i = 0; i < iterations; i++)
+								{
+									Thread.Sleep(TimeSpan.FromMilliseconds(random.Next(1000 / iterations, 15000 / iterations)));
+									action(i / (double)iterations * 100);
+								}
+							});
+					});
+				Console.WriteLine();
+				Console.WriteLine();
+			}
+			#endregion
+
+			#region Hidden ReadLine Example
+			{
+				Console.WriteLine("---------------------");
+				Console.WriteLine("This is an example of a custom \"Console.ReadLine\"");
+				Console.WriteLine("with masked characters for use cases such as passwords.");
+				Console.WriteLine("Type \"exit\" to close the program. It supports Backspace,");
+				Console.WriteLine("Delete, Left/Right Arrows, Escape, Home, and End keypresses");
+				Console.WriteLine("and the CTRL modifier.");
+				Console.Write("Input: ");
+				string input = ConsoleHelper.HiddenReadLine();
+				Console.WriteLine("Your input: " + input);
+				Console.WriteLine();
+			}
+			#endregion
+
+			#region Int Menu Example
+			{
+				Console.WriteLine("---------------------");
+				Console.WriteLine("IntMenu allows easy console menus.");
+				Console.WriteLine();
+
+				static void Option1()
+				{
+					Console.WriteLine("You chose the first option.");
+				}
+
+				static void Option2()
+				{
+					Console.WriteLine("You chose the second option.");
+				}
+
+				static void Option3()
+				{
+					Console.WriteLine("You chose the third option.");
+				}
+
+				ConsoleHelper.IntMenu(
+					("Option 1", Option1),
+					("Option 2", Option2),
+					("Option 3", Option3));
+				Console.WriteLine();
+			}
+			#endregion
+
+			#region Flush Input Buffer Example
+			{
+				Console.WriteLine("---------------------");
+				Console.WriteLine("If you want to prefent console input, you can");
+				Console.WriteLine("use FlushInputBuffer and all input prior to");
+				Console.WriteLine("calling it will be ignored.");
+				ConsoleHelper.FlushInputBuffer();
+				Console.WriteLine();
+			}
+			#endregion
+
+			Console.WriteLine("============================================");
+			Console.WriteLine("Example Complete...");
+			Console.WriteLine();
+			ConsoleHelper.PromptPressToContinue();
+		}
+
+		public static void DoWith(
+			Action action,
+			ConsoleColor? foreground = null,
+			ConsoleColor? background = null,
+			bool? cursorVisible = null)
+		{
+			ConsoleColor? foregroundRevert = null;
+			ConsoleColor? backgroundRevert = null;
+			bool? cursorVisibleRevert = null;
+			try
+			{
+				if (foreground.HasValue)
+				{
+					foregroundRevert = Console.ForegroundColor;
+					Console.ForegroundColor = foreground.Value;
+				}
+				if (background.HasValue)
+				{
+					backgroundRevert = Console.BackgroundColor;
+					Console.BackgroundColor = background.Value;
+				}
+				if (cursorVisible.HasValue)
+				{
+					try
+					{
+						cursorVisibleRevert = Console.CursorVisible;
+					}
+					catch (PlatformNotSupportedException)
+					{
+						cursorVisibleRevert = true;
+					}
+					Console.CursorVisible = cursorVisible.Value;
+				}
+				action();
+			}
+			finally
+			{
+				if (foregroundRevert.HasValue)
+				{
+					Console.ForegroundColor = foregroundRevert.Value;
+				}
+				if (backgroundRevert.HasValue)
+				{
+					Console.BackgroundColor = backgroundRevert.Value;
+				}
+				if (cursorVisibleRevert.HasValue)
+				{
+					Console.CursorVisible = cursorVisibleRevert.Value;
+				}
+			}
+		}
+	}
+}
--- Sources/Towel/ConsoleHelper.cs
+++ Sources/Towel/ConsoleHelper.cs
@@ -65,7 +65,7 @@ namespace Towel
 			}
 			prompt ??= $"Choose an option (1-{options.Length}): ";
 			invalidMessage ??= "Invalid Input. Try Again...";
-			if ((title is null))
+			if (!(title is null))
 			{
 				Console.WriteLine(title);
 			}
@@ -104,7 +104,7 @@ namespace Towel
 				? (string s, out T v) => { v = (T)(object)s; return true; }
 			: (TryParse<T>)Syntax.TryParse;
 			validation ??= v => true;
-		GetInput:
+			GetInput:
 			Console.Write(prompt ?? $"Input a {typeof(T).Name} value: ");
 			if (!tryParse(Console.ReadLine(), out T value) || !validation(value))
 			{
@@ -112,6 +112,416 @@ namespace Towel
 				goto GetInput;
 			}
 			return value;
+		}
+
+		/// <summary>Similar to <see cref="Console.ReadLine"/> but with hidden input characters.</summary>
+		/// <param name="shownCharacter">The display character to use for all input.</param>
+		/// <returns>The <see cref="string"/> input provided by the user.</returns>
+		public static string HiddenReadLine(char shownCharacter = '*')
+		{
+			System.Collections.Generic.List<char> list = new System.Collections.Generic.List<char>();
+			HiddenReadLineBase(
+				shownCharacter: shownCharacter,
+				GetLength: () => list.Count,
+				Append: list.Add,
+				InsertAt: list.Insert,
+				RemoveAt: list.RemoveAt,
+				RemoveRange: list.RemoveRange,
+				Clear: list.Clear);
+			return string.Concat(list);
+		}
+
+		internal static void HiddenReadLineBase(
+			char shownCharacter,
+			Func<int> GetLength,
+			Action<char> Append,
+			Action<int, char> InsertAt,
+			Action<int> RemoveAt,
+			Action<int, int> RemoveRange = null,
+			Action Clear = null)
+		{
+			int position = 0;
+
+			RemoveRange ??= (index, length) =>
+			{
+				for (int i = 0; i < length; i++)
+				{
+					RemoveAt(index);
+				}
+			};
+
+			Clear ??= () => RemoveRange(0, GetLength());
+
+			void MoveToOrigin() => MoveNegative(position);
+
+			void MoveToTail() => MovePositive(GetLength() - position);
+
+			while (true)
+			{
+				ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+				if (keyInfo.Key is ConsoleKey.Enter)
+				{
+					if (!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control) &&
+						!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Shift) &&
+						!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Alt))
+					{
+						MovePositive(GetLength() - position);
+						Console.WriteLine();
+						break;
+					}
+				}
+				else if (keyInfo.Key is ConsoleKey.Backspace)
+				{
+					if (keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control))
+					{
+						MoveToOrigin();
+						ConsoleWriteString(new string(shownCharacter, GetLength() - position) + new string(' ', position));
+						MoveNegative(GetLength());
+						RemoveRange(0, position);
+						position = 0;
+					}
+					else if (position > 0)
+					{
+						if (position == GetLength())
+						{
+							MoveNegative(1);
+							ConsoleWriteChar(' ');
+							MoveNegative(1);
+						}
+						else
+						{
+							MoveToTail();
+							MoveNegative(1);
+							ConsoleWriteChar(' ');
+							MoveNegative(GetLength() - position + 1);
+						}
+						RemoveAt(position - 1);
+						position--;
+					}
+				}
+				else if (keyInfo.Key is ConsoleKey.Delete)
+				{
+					if (!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control) &&
+						!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Shift) &&
+						!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Alt))
+					{
+						if (position < GetLength())
+						{
+							int left = Console.CursorLeft;
+							int top = Console.CursorTop;
+							MoveToTail();
+							MoveNegative(1);
+							ConsoleWriteChar(' ');
+							Console.CursorLeft = left;
+							Console.CursorTop = top;
+							RemoveAt(position);
+							continue;
+						}
+					}
+				}
+				else if (keyInfo.Key is ConsoleKey.Escape)
+				{
+					if (!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control) &&
+						!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Shift) &&
+						!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Alt))
+					{
+						MoveToOrigin();
+						int left = Console.CursorLeft;
+						int top = Console.CursorTop;
+						ConsoleWriteString(new string(' ', GetLength()));
+						Console.CursorLeft = left;
+						Console.CursorTop = top;
+						Clear();
+						position = 0;
+					}
+				}
+				else if (keyInfo.Key is ConsoleKey.Home)
+				{
+					if (!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Shift) &&
+						!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Alt))
+					{
+						if (keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control))
+						{
+							MoveToOrigin();
+							ConsoleWriteString(new string(shownCharacter, GetLength() - position) + new string(' ', position));
+							MoveNegative(GetLength());
+							RemoveRange(0, position);
+							position = 0;
+						}
+						else
+						{
+							MoveToOrigin();
+							position = 0;
+						}
+					}
+				}
+				else if (keyInfo.Key is ConsoleKey.End)
+				{
+					if (!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Shift) &&
+						!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Alt))
+					{
+						if (keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control))
+						{
+							MoveToOrigin();
+							ConsoleWriteString(new string(shownCharacter, position) + new string(' ', GetLength() - position));
+							MoveNegative(GetLength() - position);
+							RemoveRange(position, GetLength() - position);
+						}
+						else
+						{
+							MoveToTail();
+							position = GetLength();
+						}
+					}
+				}
+				else if (keyInfo.Key is ConsoleKey.LeftArrow)
+				{
+					if (!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Shift) &&
+						!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Alt))
+					{
+						if (keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control))
+						{
+							MoveToOrigin();
+							position = 0;
+						}
+						else
+						{
+							if (position > 0)
+							{
+								MoveNegative(1);
+								position--;
+							}
+						}
+					}
+				}
+				else if (keyInfo.Key is ConsoleKey.RightArrow)
+				{
+					if (!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Shift) &&
+						!keyInfo.Modifiers.HasFlag(ConsoleModifiers.Alt))
+					{
+						if (keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control))
+						{
+							MoveToTail();
+							position = GetLength();
+						}
+						else
+						{
+							if (position < GetLength())
+							{
+								MovePositive(1);
+								position++;
+							}
+						}
+					}
+				}
+				else
+				{
+					if (!(keyInfo.KeyChar is '\0'))
+					{
+						if (position == GetLength())
+						{
+							ConsoleWriteChar(shownCharacter);
+							Append(keyInfo.KeyChar);
+							position++;
+						}
+						else
+						{
+							int left = Console.CursorLeft;
+							int top = Console.CursorTop;
+							MoveToTail();
+							ConsoleWriteChar(shownCharacter);
+							Console.CursorLeft = left;
+							Console.CursorTop = top;
+							MovePositive(1);
+							InsertAt(position, keyInfo.KeyChar);
+							position++;
+						}
+					}
+				}
+			}
+		}
+
+		/// <summary>Animates an elipsis in the console to indicate processing.</summary>
+		/// <param name="condition">The condition of the loop.</param>
+		/// <param name="delay">The delay function.</param>
+		/// <param name="length">The length of the ellipsis.</param>
+		public static void AnimatedElipsis(
+			Func<bool> condition,
+			Action delay,
+			int length = 3)
+		{
+			_ = condition ?? throw new ArgumentNullException(nameof(condition));
+			_ = delay ?? throw new ArgumentNullException(nameof(delay));
+			if (length < 1)
+			{
+				throw new ArgumentOutOfRangeException(nameof(length), length, $"{nameof(length)} < 1");
+			}
+
+			void MoveToOrigin() => MoveNegative(length);
+
+			void Render(int frame)
+			{
+				for (int i = 0; i < frame; i++)
+				{
+					ConsoleWriteChar('.');
+				}
+				for (int i = frame; i < length; i++)
+				{
+					ConsoleWriteChar(' ');
+				}
+			}
+
+			int frame = 0;
+			Render(frame++);
+			while (condition())
+			{
+				MoveToOrigin();
+				Render(frame++);
+				delay();
+				if (frame > length)
+				{
+					frame = 0;
+				}
+			}
+			MoveToOrigin();
+			ConsoleWriteString(new string(' ', length));
+			MoveToOrigin();
+		}
+
+		/// <summary>Displays a progress bar in the console.</summary>
+		/// <param name="action">The action to track the progress of.</param>
+		/// <param name="length">The character length of the progress bar (must be >= 6).</param>
+		/// <param name="header">The header character of the progress bar.</param>
+		/// <param name="footer">The footer character of the progress bar.</param>
+		/// <param name="done">The character for represening completed progress.</param>
+		/// <param name="remaining">The character representing ongoing processing.</param>
+		/// <param name="errorDigit">The characters to display in the numerical display when an invalid percentage is recieved.</param>
+		/// <param name="postClear">Whether or not to clear the progress bar from the view when complete.</param>
+		public static void ProgressBar(
+			Action<Action<double>> action,
+			int length = 17,
+			char header = '[',
+			char footer = ']',
+			char done = '=',
+			char remaining = '-',
+			char errorDigit = '?',
+			bool postClear = true)
+		{
+			_ = action ?? throw new ArgumentNullException(nameof(action));
+			if (length < 6)
+			{
+				throw new ArgumentOutOfRangeException(nameof(length), length, $"{length} < 6");
+			}
+
+			void MoveToOrigin() => MoveNegative(length);
+
+			void Render(double percentage)
+			{
+				ConsoleWriteChar(header);
+				if (percentage < 0 || percentage > 100)
+				{
+					ConsoleWriteString(new string(remaining, length - 7));
+				}
+				else
+				{
+					int doneCount = (int)(percentage / 100 * (length - 7));
+					int remainingCount = length - 7 - doneCount;
+					ConsoleWriteString(new string(done, doneCount));
+					ConsoleWriteString(new string(remaining, remainingCount));
+				}
+				ConsoleWriteChar(footer);
+				ConsoleWriteChar(' ');
+				if (percentage < 0 || percentage > 100)
+				{
+					ConsoleWriteString(new string(errorDigit, 2));
+					ConsoleWriteChar('%');
+					ConsoleWriteChar(' ');
+				}
+				else
+				{
+					string percentString = ((int)(percentage)).ToString(System.Globalization.CultureInfo.InvariantCulture);
+					ConsoleWriteString(percentString);
+					ConsoleWriteChar('%');
+					for (int i = percentString.Length; i < 3; i++)
+					{
+						ConsoleWriteChar(' ');
+					}
+				}
+			}
+
+			Render(0);
+			action(percent => { MoveToOrigin(); Render(percent); });
+			if (postClear)
+			{
+				MoveToOrigin();
+				ConsoleWriteString(new string(' ', length));
+				MoveToOrigin();
+			}
+			else
+			{
+				MoveToOrigin();
+				Render(100);
+			}
+		}
+
+		internal static void MoveNegative(int count)
+		{
+			int bufferWidth = Console.BufferWidth;
+			int left = Console.CursorLeft;
+			int top = Console.CursorTop;
+			for (int i = 0; i < count; i++)
+			{
+				if (left > 0)
+				{
+					left--;
+				}
+				else
+				{
+					top--;
+					left = bufferWidth - 1;
+				}
+			}
+			Console.CursorLeft = left;
+			Console.CursorTop = top;
+		}
+
+		internal static void MovePositive(int count)
+		{
+			int bufferWidth = Console.BufferWidth;
+			int left = Console.CursorLeft;
+			int top = Console.CursorTop;
+			for (int i = 0; i < count; i++)
+			{
+				if (left == bufferWidth - 1)
+				{
+					top++;
+					left = 0;
+				}
+				else
+				{
+					left++;
+				}
+			}
+			Console.CursorLeft = left;
+			Console.CursorTop = top;
+		}
+
+		internal static void ConsoleWriteChar(char @char)
+		{
+			int temp = Console.CursorLeft;
+			Console.Write(@char);
+			if (Console.CursorLeft == temp)
+			{
+				MovePositive(1);
+			}
+		}
+
+		internal static void ConsoleWriteString(string @string)
+		{
+			foreach (char c in @string)
+			{
+				ConsoleWriteChar(c);
+			}
 		}
 	}
 }
