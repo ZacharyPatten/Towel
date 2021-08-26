d98f8bfde5a6ae86a01063e3f5ce8cca728a9e5e
Zachary Patten <sevenix.zp@gmail.com>
2021-08-25 23:33:21 -0400

    SStringBuilder tests

--- Tools/Towel_Testing/SStringBuilder.cs
+++ Tools/Towel_Testing/SStringBuilder.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Towel;
 using static Towel.Statics;
 
 namespace Towel_Testing
@@ -12,52 +11,86 @@ namespace Towel_Testing
 		public void Testing()
 		{
 			{
-				SStringBuilder span = Span<char>.Empty;
-				Assert.IsTrue(span._stringBuilder is null);
-				span.Append('a');
-				Assert.IsTrue(span._stringBuilder is not null);
-				Assert.IsTrue(EquateSequence<char>((string)span, "a"));
+				SStringBuilder sb = Span<char>.Empty;
+				Assert.IsTrue(sb._stringBuilder is null);
+				sb.Append('a');
+				Assert.IsTrue(sb._stringBuilder is not null);
+				Assert.IsTrue(EquateSequence<char>((string)sb, "a"));
 			}
 			{
-				SStringBuilder span = stackalloc char[3];
-				Assert.IsTrue(span._stringBuilder is null);
-				span.Append('a');
-				span.Append('b');
-				span.Append('c');
-				Assert.IsTrue(EquateSequence<char>((string)span, "abc"));
-				span.Append('d');
-				Assert.IsTrue(span._stringBuilder is not null);
-				Assert.IsTrue(EquateSequence<char>((string)span, "abcd"));
+				SStringBuilder sb = stackalloc char[3];
+				Assert.IsTrue(sb._stringBuilder is null);
+				sb.Append('a');
+				sb.Append('b');
+				sb.Append('c');
+				Assert.IsTrue(EquateSequence<char>((string)sb, "abc"));
+				sb.Append('d');
+				Assert.IsTrue(sb._stringBuilder is not null);
+				Assert.IsTrue(EquateSequence<char>((string)sb, "abcd"));
 			}
 			{
-				SStringBuilder span = stackalloc char[3];
-				Assert.IsTrue(span._stringBuilder is null);
-				span.Append("abc");
-				Assert.IsTrue(EquateSequence<char>((string)span, "abc"));
-				span.Append('d');
-				Assert.IsTrue(span._stringBuilder is not null);
-				Assert.IsTrue(EquateSequence<char>((string)span, "abcd"));
+				SStringBuilder sb = stackalloc char[3];
+				Assert.IsTrue(sb._stringBuilder is null);
+				sb.Append("abc");
+				Assert.IsTrue(EquateSequence<char>((string)sb, "abc"));
+				sb.Append('d');
+				Assert.IsTrue(sb._stringBuilder is not null);
+				Assert.IsTrue(EquateSequence<char>((string)sb, "abcd"));
 			}
 			{
-				SStringBuilder span = stackalloc char[3];
-				Assert.IsTrue(span._stringBuilder is null);
-				span.Append("abcd");
-				Assert.IsTrue(span._stringBuilder is not null);
-				Assert.IsTrue(EquateSequence<char>((string)span, "abcd"));
+				SStringBuilder sb = stackalloc char[3];
+				Assert.IsTrue(sb._stringBuilder is null);
+				sb.Append("abcd");
+				Assert.IsTrue(sb._stringBuilder is not null);
+				Assert.IsTrue(EquateSequence<char>((string)sb, "abcd"));
 			}
 			{
-				SStringBuilder span = stackalloc char[1];
-				Assert.IsTrue(span._stringBuilder is null);
-				span.AppendLine('a');
-				Assert.IsTrue(span._stringBuilder is not null);
-				Assert.IsTrue(EquateSequence<char>((string)span, "a" + Environment.NewLine));
+				SStringBuilder sb = stackalloc char[1];
+				Assert.IsTrue(sb._stringBuilder is null);
+				sb.AppendLine('a');
+				Assert.IsTrue(sb._stringBuilder is not null);
+				Assert.IsTrue(EquateSequence<char>((string)sb, "a" + Environment.NewLine));
 			}
 			{
-				SStringBuilder span = stackalloc char[3];
-				Assert.IsTrue(span._stringBuilder is null);
-				span.AppendLine("abc");
-				Assert.IsTrue(span._stringBuilder is not null);
-				Assert.IsTrue(EquateSequence<char>((string)span, "abc" + Environment.NewLine));
+				SStringBuilder sb = stackalloc char[3];
+				Assert.IsTrue(sb._stringBuilder is null);
+				sb.AppendLine("abc");
+				Assert.IsTrue(sb._stringBuilder is not null);
+				Assert.IsTrue(EquateSequence<char>((string)sb, "abc" + Environment.NewLine));
+			}
+			{
+				SStringBuilder sb = stackalloc char[1];
+				Assert.IsTrue(sb._stringBuilder is null);
+				sb.Append("abc");
+				sb.Append("def");
+				Assert.IsTrue(sb._stringBuilder is not null);
+				Assert.IsTrue(EquateSequence<char>((string)sb, "abcdef"));
+			}
+			{
+				SStringBuilder sb = stackalloc char[1];
+				Assert.IsTrue(sb._stringBuilder is null);
+				sb.Append('a');
+				sb.Append('b');
+				Assert.IsTrue(EquateSequence<char>(sb.ToString(), "ab"));
+			}
+			{
+				SStringBuilder sb = stackalloc char[10];
+				Assert.IsTrue(sb._stringBuilder is null);
+				sb.Append("abc");
+				Assert.IsTrue(EquateSequence<char>(sb.ToString(), "abc"));
+			}
+			{
+				SStringBuilder sb = stackalloc char[1];
+				Assert.IsTrue(sb._stringBuilder is null);
+				sb.Append("abc");
+				sb.Append("def");
+				Assert.IsTrue(EquateSequence<char>(sb.ToString(), "abcdef"));
+			}
+			{
+				SStringBuilder sb = stackalloc char[10];
+				Assert.IsTrue(sb._stringBuilder is null);
+				sb.AppendLine();
+				Assert.IsTrue(EquateSequence<char>(sb.ToString(), Environment.NewLine));
 			}
 		}
 	}
